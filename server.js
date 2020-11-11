cb07ee95f0a50ac1690fa1761ef9029968ce909c
alessiaarosio <alessiarosio.aa@gmail.com>
2020-11-11 10:11:07 +0100

    project_01

---
+++ server.js
@@ -0,0 +1,2 @@
+// questo è il file che viene avviato dal server
+console.log("node is running") // se andiamo su github desktop - repositoyr - open in command prompt, ci fa vedere se la pagina viene elaborata dal server scrivendo "node server.js" (senza % prima della frase)
