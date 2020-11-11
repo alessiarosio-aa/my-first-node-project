34d3ceeeaed88d8e842cac69f8f6c1ef4d420dd8
alessiaarosio <alessiarosio.aa@gmail.com>
2020-11-11 10:27:49 +0100

    added express

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
 // questo è il file che viene avviato dal server
-console.log("node is running") // se andiamo su github desktop - repositoyr - open in command prompt, ci fa vedere se la pagina viene elaborata dal server scrivendo "node server.js" (senza % prima della frase)
+console.log("node is running") // se andiamo su github desktop - repository - open in command prompt, ci fa vedere se la pagina viene elaborata dal server scrivendo "node server.js" (senza % prima della frase)
+
+// scrivendo nel terminal npm init ci connette al serve (premo invio senza inserire i dati per ora non ci interesssa)
+// c'è un oggetto json che ci permette di avviare il progetto, viene quindi creato un file che si chiama package.json, in questo modo possiamo installare i packages
+// scrivendo nel terminal npm install expres--save mi installa il package express
+// questo crea un nuovo file chiamato pakage-lock.json
